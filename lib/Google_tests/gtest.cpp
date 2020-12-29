795401bce4ea09be84c939ef0a30b506c3a32e55
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2020-12-29 14:26:20 +0100

    Add google test

---
+++ lib/Google_tests/gtest.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Ny Andrianina Mamy on 20/12/2020.
+//
+
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
