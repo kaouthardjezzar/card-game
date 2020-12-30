ab2292370b0112be1f6e86f3583778150ceb37b3
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2020-12-30 15:10:45 +0100

    Implement cin utils

--- examples/GameTemplate.h
+++ examples/GameTemplate.h
@@ -26,6 +26,24 @@ protected:
 
     virtual void end_of_game() = 0;
 
+    // Utils
+    template<typename T>
+    T ask_player(const std::string& title)
+    {
+        T x = 0;
+        std::cout << title;
+        while (!( std::cin >> x))
+        {
+            std::cin.clear();
+            std::cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Veuillez entrer un nombre/mot correct: " << endl;
+            std::cout << title;
+        }
+        std::cin.clear();
+        std::cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        return (x);
+    }
+
 public:
     void lets_play();
 };
