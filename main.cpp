1ab8c241d5f458551d4cfd6ed99bac80e608d3c5
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2021-01-10 14:09:08 +0100

    Implement game choice

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "examples/Bataille.h"
+#include "examples/Uno.h"
 #include "examples/HuitAmericain.h"
+#include "examples/Scopa.h"
 #include "examples/Briscola.h"
 
 
@@ -14,12 +16,53 @@ int main() {
 
     logd(LOG_DEBUG) << "Game is starting ";
 
-    cout << "Hello, World!" << endl;
-    HuitAmericain huitam;
-    Bataille bataille;
-    Briscola briscola;
-    huitam.lets_play();
-    //bataille.lets_play();
-    //briscola.lets_play();
+    cout << "Bienvenue dans le jeu, " << endl;
+    cout << "Nous vous invitons à choisir un des jeux ci dessous, " << endl;
+
+    vector<string> games = {"Bataille", "Uno", "8 Americain", "Scopa", "Briscola"};
+
+    for(int i=1; i <= (int)games.size(); ++i) {
+        cout << i << " " << games[i] << endl;
+    }
+
+    int choice = -1;
+
+    while(choice < 1 && choice > (int)games.size()) {
+        cout << "Votre choix : ";
+        cin >> choice;
+    }
+
+    switch (choice) {
+        case 1: {
+            Bataille game;
+            game.lets_play();
+            break;
+        }
+        case 2: {
+            Uno game;
+            game.lets_play();
+            break;
+        }
+        case 3: {
+            HuitAmericain game;
+            game.lets_play();
+            break;
+        }
+        case 4: {
+            Scopa game;
+            game.lets_play();
+            break;
+        }
+        case 5: {
+            Briscola game;
+            game.lets_play();
+            break;
+        }
+        default:
+            cout << "Aucun jeu n'a été lancé" << endl;
+            break;
+    }
+
+    cout << "\n Au revoir et merci beaucoup " << endl;
     return 0;
 }
